fd37ca1f6218feef9a96b03808e3438740b258c5
gayujesupaul <141612283+gayujesupaul@users.noreply.github.com>
2024-07-26 16:31:57 +0530

    Add files via upload

---
+++ ATM.java
@@ -0,0 +1,56 @@
+package atm;
+import java.util.Scanner;
+
+public class ATM {
+    private BankAccount account;
+    private Scanner scanner;
+
+    public ATM(BankAccount account) {
+        this.account = account;
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void start() {
+        while (true) {
+            showMenu();
+            int choice = scanner.nextInt();
+            handleChoice(choice);
+        }
+    }
+
+    private void showMenu() {
+        System.out.println("Welcome to the ATM");
+        System.out.println("1. Check Balance");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Exit");
+        System.out.print("Please choose an option: ");
+    }
+
+    private void handleChoice(int choice) {
+        switch (choice) {
+            case 1:
+                account.checkBalance();
+                break;
+            case 2:
+                System.out.print("Enter deposit amount: ");
+                double depositAmount = scanner.nextDouble();
+                account.deposit(depositAmount);
+                break;
+            case 3:
+                System.out.print("Enter withdrawal amount: ");
+                double withdrawAmount = scanner.nextDouble();
+                account.withdraw(withdrawAmount);
+                break;
+            case 4:
+                System.out.println("Thank you for using the ATM. Goodbye!");
+                System.exit(0);
+                break;
+            default:
+                System.out.println("Invalid choice. Please try again.");
+        }
+    }
+}
+
+
+
---
+++ Main.java
@@ -0,0 +1,9 @@
+package atm;
+public class Main {
+    public static void main(String[] args) {
+        BankAccount account = new BankAccount(1000); // Initial balance
+        ATM atm = new ATM(account);
+        atm.start();
+    }
+}
+
